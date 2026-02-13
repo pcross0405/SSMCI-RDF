68f0bcc9c2a6352ce41562d3a0050445a5983e95
Tyler Knapp <tylerrknapp@gmail.com>
2026-02-13 12:21:07 -0500

    Switch to KDTree
    The rdf functions have been rewritten to use scipy's KDTree function, which is ~2.5 times faster than the custom code.

--- src/particle_rdf.py
+++ src/particle_rdf.py
@@ -1,25 +1,30 @@
 import numpy as np
+from scipy.spatial import KDTree
 
 def FrameImporter(filename, frame):
     """
-    Extracts the position data for the given frame range.
+    Generator that extracts the position data for the given frame range. Each 
+    time the function is called, the next frame will be output.
     
     Parameters
     ----------
     filename : string
         The path to the position file
     frame : int or sequence of ints
-        The target frame or sequence of frames to be extracted. If a sequence is used, all frames must be contiguous
+        The target frame or sequence of frames to be extracted. If a sequence 
+        is used, all frames must be contiguous
         
     Yields
     ----------
     positionArray : nd.array
-        A numpy array containing the positions of each particle in a frame. The ID of the particle is the index of the array.
+        A numpy array containing the positions of each particle in a frame. The 
+        ID of the particle is the index of the array.
     """
     
     with open(filename) as f:        
         
-        # Get the number of atoms in the simulation as well as simulation dimension (assume cube)
+        # Get the number of atoms in the simulation as well as simulation 
+        # dimension (assume cube)
         
         atomCount = 0
         simDimension = 0
@@ -31,7 +36,7 @@ def FrameImporter(filename, frame):
                 simDimension = float(f.readline().split(" ")[1])
                 break
         
-        # Move through the file and collect the position data for the given frames
+        # Move through the file and collect the position data for each frame
         
         f.seek(0)
         positionArray = np.empty((atomCount, 3))
@@ -71,12 +76,14 @@ def FrameImporter(filename, frame):
 
 def ParticleRDF(positionArray, targetID, binCount=1000, maxRange=20):
     """
-    Calculates the histogram of distances between the target particle and every other particle in a frame.
+    Calculates the histogram of distances between the target particle and every 
+    other particle in a frame.
     
     Parameters
     ----------
     positionArray : nd.array
-        numpy array containing the position data of each particle. The ID should be the index.
+        numpy array containing the position data of each particle. The ID should
+        be the index.
     targetID : int
         The ID of the particle around which the rdf will be calculated.
     binCount : int, default=1000
@@ -101,26 +108,66 @@ def ParticleRDF(positionArray, targetID, binCount=1000, maxRange=20):
         
         distanceArray[c] = distance
     
-    histValues, binEdges = np.histogram(distanceArray, bins=binCount, density=True, range=(0, maxRange))
+    histValues, binEdges = np.histogram(distanceArray, bins=binCount, 
+                                        density=True, range=(0, maxRange))
+    
+    return (histValues, binEdges)
+
+def TreeRDF(positionArray, targetID, binCount=1000, maxRange=20):
+    """
+    Calculates the histogram of distances between the target particle and every 
+    other particle in a frame.
+    
+    Parameters
+    ----------
+    positionArray : nd.array
+        numpy array containing the position data of each particle. The ID should
+        be the index.
+    targetID : int
+        The ID of the particle around which the rdf will be calculated.
+    binCount : int, default=1000
+        The number of bins in the rdf, defaults to 1000.
+    maxRange : float, default=20
+        The distance in angstroms the rdf should be calculated to.
+    
+    Returns
+    ----------
+    histValues : nd.array
+        The values of each bins, normalized to a sum of 1.
+    binEdges : nd.array of dtype float
+        The edges of the histogram bins.
+    """
+    tree = KDTree(positionArray)
+    
+    indexArray = tree.query_ball_point(positionArray[targetID], maxRange)
+    
+    distanceArray = np.linalg.norm(positionArray[indexArray] - positionArray[targetID], axis=1)
+    
+    histValues, binEdges = np.histogram(distanceArray, bins=binCount, 
+                                        density=True, range=(0, maxRange))
     
     return (histValues, binEdges)
 
 def GeneratorWrapper(filename, frames, targetID):
     """
-    Wrapper to minimize memory usage while generating the rdf for a particle over a range of frames. Does not save the position data after calculating the rdf
+    Wrapper to minimize memory usage while generating the rdf for a particle 
+    over a range of frames. Does not save the position data after calculating
+    the rdf
     
     Parameters
     ----------
     filename : string
         The path to the position file
     frame : int or sequence of ints
-        The target frame or sequence of frames to be extracted. If a sequence is used, all frames must be contiguous
+        The target frame or sequence of frames to be extracted. If a sequence
+        is used, all frames must be contiguous
     targetID : int
         The ID of the particle around which the rdf will be calculated.    
     Returns
     ----------
     histValuesList : List of nd.arrays
-        A list containing the values of each bins, normalized to a sum of 1, for each frame
+        A list containing the values of each bins, normalized to a sum of 1, 
+        for each frame
     binEdgesList : List of nd.arrays of dtype float
         A list containing the edges of the histogram bins for each frame
     """
@@ -136,21 +183,63 @@ def GeneratorWrapper(filename, frames, targetID):
         histValuesList.append(histValues)
         binEdgesList.append(binEdges)
 
-        print(f"{(i+1)/len(frames)*100:5.2f}" + "% complete", end='\r')
+        # print(f"{(i+1)/len(frames)*100:5.2f}" + "% complete", end='\r')
     
     return (histValuesList, binEdgesList)
 
-def GeneratorWrapperGaussian(filename, frames, targetID):
+def GeneratorWrapperTree(filename, frames, targetID):
     """
-    Wrapper to minimize memory usage while generating the rdf for a particle over a range of frames.
-    Does not save the position data after calculating the rdf. Additionally, points are broadened using gaussians and summed together.
+    Wrapper to minimize memory usage while generating the rdf for a particle 
+    over a range of frames. Does not save the position data after calculating
+    the rdf
     
     Parameters
     ----------
     filename : string
         The path to the position file
     frame : int or sequence of ints
-        The target frame or sequence of frames to be extracted. If a sequence is used, all frames must be contiguous
+        The target frame or sequence of frames to be extracted. If a sequence
+        is used, all frames must be contiguous
+    targetID : int
+        The ID of the particle around which the rdf will be calculated.    
+    Returns
+    ----------
+    histValuesList : List of nd.arrays
+        A list containing the values of each bins, normalized to a sum of 1, 
+        for each frame
+    binEdgesList : List of nd.arrays of dtype float
+        A list containing the edges of the histogram bins for each frame
+    """
+    frameGenerator = FrameImporter(filename, frames)
+    
+    histValuesList = []
+    binEdgesList = []
+
+    for i in range(len(frames)):
+        positions = next(frameGenerator)
+
+        histValues, binEdges = TreeRDF(positions, targetID, binCount=500)
+        histValuesList.append(histValues)
+        binEdgesList.append(binEdges)
+
+        # print(f"{(i+1)/len(frames)*100:5.2f}" + "% complete", end='\r')
+    
+    return (histValuesList, binEdgesList)
+
+def GeneratorWrapperGaussian(filename, frames, targetID):
+    """
+    Wrapper to minimize memory usage while generating the rdf for a particle 
+    over a range of frames. Does not save the position data after calculating 
+    the rdf. Additionally, points are broadened using gaussians 
+    and summed together.
+    
+    Parameters
+    ----------
+    filename : string
+        The path to the position file
+    frame : int or sequence of ints
+        The target frame or sequence of frames to be extracted. If a sequence 
+        is used, all frames must be contiguous
     targetID : int
         The ID of the particle around which the rdf will be calculated.    
     Returns
@@ -172,6 +261,57 @@ def GeneratorWrapperGaussian(filename, frames, targetID):
         for c, vector2 in enumerate(positions):
             dist = np.linalg.norm(vector1-vector2)
             distanceArray[c] = dist
+        
+        x = np.linspace(0, 20, 5000)
+        y = np.zeros(5000)
+        
+        sigma = 0.05
+        gaussianFunc = lambda x, mu : np.exp(-((x-mu)**2)/(2*sigma**2))
+        for point in distanceArray:
+            
+            y += gaussianFunc(x, point)
+            
+        yList.append(y)
+        print(f"{(i+1)/len(frames)*100:5.2f}" + "% complete", end='\r')
+        
+    
+    return (x, yList)
+
+def GeneratorWrapperGaussianTree(filename, frames, targetID):
+    """
+    Wrapper to minimize memory usage while generating the rdf for a particle 
+    over a range of frames. Does not save the position data after calculating 
+    the rdf. Additionally, points are broadened using gaussians 
+    and summed together.
+    
+    Parameters
+    ----------
+    filename : string
+        The path to the position file
+    frame : int or sequence of ints
+        The target frame or sequence of frames to be extracted. If a sequence 
+        is used, all frames must be contiguous
+    targetID : int
+        The ID of the particle around which the rdf will be calculated.    
+    Returns
+    ----------
+    x : nd.array
+        Array covering the x axis containing 5000 points.
+    yList : List of nd.arrays of dtype float
+        A list containing the gaussian broadened signal at each distance.
+    """
+    
+    frameGenerator = FrameImporter(filename, frames)
+    
+    yList = []
+
+    for i in range(len(frames)):
+        positions = next(frameGenerator)
+        tree = KDTree(positions)
+    
+        indexArray = tree.query_ball_point(positions[targetID], 20)
+    
+        distanceArray = np.linalg.norm(positions[indexArray] - positions[targetID], axis=1)
         
         x = np.linspace(0, 20, 5000)
         y = np.zeros(5000)
